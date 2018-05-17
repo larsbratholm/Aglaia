dbb30f490ad107fc43f196019b970770eab368cf
larsbratholm <larsbratholm@gmail.com>
2018-05-17 12:19:10 +0100

    daily

--- aglaia/dimensionality_reduction.py
+++ aglaia/dimensionality_reduction.py
@@ -7,7 +7,7 @@ import numpy as np
 #import matplotlib.pyplot as plt
 import tensorflow as tf
 
-from .utils import InputError, is_positive_integer
+from .utils import InputError, is_positive_integer, ceil
 
 class VAE(object):
     """
@@ -19,10 +19,41 @@ class VAE(object):
 
     tfd = tf.contrib.distributions
 
-    def __init__(self, dimensions = 2, layer_sizes = [200,200], learning_rate = 0.001):
+    def __init__(self, dimensions = 2, layer_sizes = [200,200], learning_rate = 0.001,
+            activation_function = tf.nn.relu, n_samples = 1, n_iterations = 20, batch_size = 100):
         self.dimensions = dimensions
         self.layer_sizes = layer_sizes
         self.learning_rate = learning_rate
+        self.activation_function = activation_function
+        self.n_samples = n_samples
+        self.n_iterations = n_iterations
+        self.batch_size = batch_size
+
+    def _get_batch_size(self):
+        """
+        Determines the actual batch size.
+        If the batch size is larger than the number of samples, it is truncated and a warning
+        is printed.
+
+        Furthermore the returned batch size will be slightly modified from the user input if
+        the last batch would be tiny compared to the rest.
+
+        :return: Batch size
+        :rtype: int
+        """
+
+        if self.batch_size > self.n_samples:
+            print("Warning: batch_size larger than sample size. It is going to be clipped")
+            return min(self.n_samples, self.batch_size)
+        else:
+            batch_size = self.batch_size
+
+        # see if the batch size can be modified slightly to make sure the last batch is similar in size
+        # to the rest of the batches
+        # This is always less that the requested batch size, so no memory issues should arise
+        better_batch_size = ceil(self.n_samples, ceil(self.n_samples, batch_size))
+
+        return better_batch_size
 
     def _make_encoder(self, data):
         """
@@ -31,11 +62,11 @@ class VAE(object):
         """
         x = tf.layers.flatten(data)
         for layer_size in self.layer_sizes:
-            x = tf.layers.dense(x, layer_size, tf.nn.relu)
+            x = tf.layers.dense(x, layer_size, self.activation_function)
 
         loc = tf.layers.dense(x, self.dimensions)
         scale = tf.layers.dense(x, self.dimensions, tf.nn.softplus)
-        return tfd.MultivariateNormalDiag(loc, scale)
+        return tfd.MultivariateNormalDiag(loc, scale), loc
 
 
     def _make_prior(self):
@@ -56,61 +87,82 @@ class VAE(object):
         ReLU is applied on the output layer since all input
         features are assumed positive, but exp could be used
         as well.
-        A Deterministic distribution is used to get the entropy,
-        but a full- or half-normal or something similar might be better.
+        Returns a gamma distribution since input is positive or zero.
+        A mixture might be better since the input is sparse.
+        Alternatively a beta-like distribution could be used if the max/min of the output
+        was stored or the input was normalised.
 
         """
 
         for layer_size in self.layer_sizes:
-            x = tf.layers.dense(x, layer_size, tf.nn.relu)
+            x = tf.layers.dense(x, layer_size, self.activation_function)
 
-        x = tf.layers.dense(x, self.n_features, tf.nn.relu)
-        x = tf.reshape(x, [-1, self.n_features])
-        # TODO 
-        # pretty sure deterministic will only work for 0 values
-        return tfd.Independent(tfd.Deterministic(x), 1)
+        #x = tf.layers.dense(x, self.n_features, tf.nn.relu)
+        alpha = tf.layers.dense(x, self.dimensions, tf.nn.softplus)
+        beta = tf.layers.dense(x, self.dimensions, tf.nn.softplus)
+        return tfd.Independent(tfd.GammaDistribution(alpha, beta), 1)
 
     def fit(self, filenames):
 
         # TODO create slatm for _fit function
 
-    def _fit(self, x):
+        return self._fit(x, x_test)
+
+    def _fit(self, x, x_test = None):
 
         self.n_samples = x.shape[0]
         self.n_features = x.shape[1]
+	batch_size = self._get_batch_size()
+	n_batches = ceil(self.n_samples, batch_size)
 
         data = tf.placeholder(tf.float32, [None, self.n_features])
+
+	# Create the dataset iterator
+        dataset = tf.data.Dataset.from_tensor_slices(data)
+        dataset = dataset.shuffle(buffer_size = self.n_samples)
+        dataset = dataset.batch(batch_size)
+        iterator = tf.data.Iterator.from_structure(dataset.output_types, dataset.output.shapes)
+        tf_data = iterator.get_next()
 
         make_encoder = tf.make_template('encoder', self._make_encoder)
         make_decoder = tf.make_template('decoder', self._make_decoder)
 
         # Define the model.
         prior = self._make_prior()
-        posterior = make_encoder(data)
-        code = posterior.sample()
+        posterior, post_means = make_encoder(data)
+        code = posterior.sample(self.n_samples)
 
         # Define the loss.
-        # likelihood ~ E[log(P(X|z))]
+        # likelihood ~ E[log(P(X|z))]. This is approximated by a single sample
+        # of z. In principle we could draw several samples to get a better approximation.
         likelihood = make_decoder(code).log_prob(data)
         # divergence ~ D_KL[Q(z|X) || P(z)] 
         divergence = tfd.kl_divergence(posterior, prior)
         # elbo ~ lower bound of log(P(X))
-        elbo = tf.reduce_mean(likelihood - divergence)
+        elbo = tf.reduce_mean(likelihood / self.n_samples - divergence)
         optimize = tf.train.AdamOptimizer(self.learning_rate).minimize(-elbo)
 
         #samples = make_decoder(prior.sample(10), [28, 28]).mean()
 
-        mnist = input_data.read_data_sets('MNIST_data/')
-        fig, ax = plt.subplots(nrows=20, ncols=11, figsize=(10, 20))
+        # Initialize
+        init = tf.global_variables_initializer()
+        iterator_init = iterator.make_initializer(dataset)
+
         with tf.train.MonitoredSession() as sess:
-          for epoch in range(20):
-            feed = {data: mnist.test.images.reshape([-1, 28, 28])}
-            test_elbo, test_codes, test_samples = sess.run([elbo, code, samples], feed)
-            plot_codes(ax[epoch, 0], test_codes, mnist.test.labels)
-            plot_samples(ax[epoch, 1:], test_samples)
-            for _ in range(600):
-              feed = {data: mnist.train.next_batch(100)[0].reshape([-1, 28, 28])}
-              sess.run(optimize, feed)
+            sess.run(init)
+            for it in range(self.n_iterations):
+                test_cost = sess.run(elbo, feed_dict = {data:x_test})
+                print("Test cost at iteration %d: %6.3f" % (it+1, test_cost))
+                sess.run(iterator_init, feed_dict={data:x})
+                for _ in range(n_batches):
+                    sess.run(optimize)
+            sess.run(iterator_init, feed_dict={data:x})
+            z_means = []
+            for _ in range(n_batches):
+                z_means_batch = sess.run(post_means)
+                z_means.append(z_means_batch)
+
+        return z_means
 
     def __init__(self, representation = 'unsorted_coulomb_matrix', 
             slatm_sigma1 = 0.05, slatm_sigma2 = 0.05, slatm_dgrid1 = 0.03, slatm_dgrid2 = 0.03, slatm_rcut = 4.8, slatm_rpower = 6,
